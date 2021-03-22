8c35f22697629bc7caca5ff22dc9ac77d64e8d81
Himalaya <himalaya.gupta3@gmail.com>
2021-03-22 11:22:37 +0530

    Slider Changed

--- content/js/main.js
+++ content/js/main.js
@@ -4,7 +4,6 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -12,11 +11,8 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3500); // Change image every 3.5 seconds
 }
 //
